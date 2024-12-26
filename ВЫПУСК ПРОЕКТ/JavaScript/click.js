8dda6fd80acda43f760beae976f52b097a5771a4
Makey Julian <makeyjulian@gmail.com>
2024-12-26 15:28:26 +0300

    PROECT

---
+++ ВЫПУСК ПРОЕКТ/JavaScript/click.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $(".btn1").click(function () {
+        $(".option1").toggle('show')
+        $(".non1").toggle('show')
+        $(".yes1").toggle('.non1')
+    })
+    $(".btn2").click(function () {
+        $(".option2").toggle('show')
+        $(".non2").toggle('show')
+        $(".yes2").toggle('.non2')
+    })
+    $(".btn3").click(function () {
+        $(".option3").toggle('show')
+        $(".non3").toggle('show')
+        $(".yes3").toggle('.non3')
+    })
+    $(".btn4").click(function () {
+        $(".option4").toggle('show')
+        $(".non4").toggle('show')
+        $(".yes4").toggle('.non4')
+    })
+    $(".btn5").click(function () {
+        $(".option5").toggle('show')
+        $(".non5").toggle('show')
+        $(".yes5").toggle('.non5')
+    })
+})
